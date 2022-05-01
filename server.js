c224f020f328c15f7d8be6da7002648cf36c0392
Tural Hajiyev <thajiyev9@outlook.com>
2022-05-01 17:23:24 +0200

    Add cors policy to allow foreign access

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors')
 const path = require('path');
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
+app.use(cors())
 app.use(express.json());
 
 // // Define Routes
